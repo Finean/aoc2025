187f3290704a079e927a94ba5b9c7fb4ef2e9cd4
Finean <fin.chetham@gmail.com>
2025-12-02 17:14:30 +0000

    Add files via upload

---
+++ day_2/src/main.rs
@@ -0,0 +1,150 @@
+use std::fs;
+use std::collections::HashSet;
+
+fn main() -> std::io::Result<()> {
+    let content = fs::read_to_string("input.txt")?;
+    let ranges: Vec<&str> = content.trim().split(',').collect();
+
+    println!("{:?}", ranges);
+
+    let mut sum = 0;
+    let mut sum_2 = 0;
+    for r in ranges {
+        let parts: Vec<&str> = r.split('-').collect();
+        if parts.len() == 2 {
+            println!("{:?}", parts);
+            let start: u64 = parts[0].parse().unwrap();
+            let end: u64 = parts[1].parse().unwrap();
+            let start_len = (parts[0].len() - 1) as u32;
+            let end_len = (parts[1].len() - 1) as u32;
+
+            sum += p1_invalid_sum(start, end, start_len, end_len);
+            sum_2 += p2_invalid_sum(start, end, start_len, end_len);
+        }
+    }
+    println!("Part 1 Sum: {}", sum);
+    println!("Part 2 Sum: {}", sum_2);
+    Ok(())
+
+}
+
+
+fn p1_invalid_sum(min: u64, max: u64, base_min: u32, base_max: u32) -> u64 {
+    let mut sum: u64 = 0;
+
+    if base_min == base_max {
+        if base_min % 2 == 0 {
+            return 0;
+        }
+        let i_min = min / 10u64.pow(1 + (base_min / 2));
+        let i_max = max / 10u64.pow(1 + (base_max / 2));
+
+        if i_min == i_max {
+            let val = i_min * 10u64.pow(1 + base_min / 2) + i_min;
+            if min <= val && val <= max {
+                return val;
+            } else {
+                return 0;
+            }
+        } else {
+            sum += sum_inv(i_min, i_max, base_min);
+        }
+
+        let val_min = i_min * 10u64.pow(1 + base_min / 2) + i_min;
+        let val_max = i_max * 10u64.pow(1 + base_max / 2) + i_max;
+
+        if val_min < min {
+            sum -= val_min;
+        }
+        if val_max > max {
+            sum -= val_max;
+        }
+        return sum;
+    }
+
+    for base in base_min..=base_max {
+        if base % 2 == 0 {
+            continue;
+        }
+        if base == base_min {
+            let i_min = min / 10u64.pow(1 + base_min / 2);
+            let i_max = 10u64.pow(1 + base / 2) - 1;
+            sum += sum_inv(i_min, i_max, base);
+        } else if base == base_max {
+            let i_min = 10u64.pow(base / 2);
+            let i_max = max / 10u64.pow(1 + base_max / 2);
+            sum += sum_inv(i_min, i_max, base);
+        } else {
+            let i_min = 10u64.pow(base / 2);
+            let i_max = i_min * 10 - 1;
+            sum += sum_inv(i_min, i_max, base);
+        }
+    }
+    sum
+}
+
+fn sum_inv(min: u64, max: u64, base: u32) -> u64 {
+    let mut sum: u64 = 0;
+    for i in min..=max {
+        let val = i * 10u64.pow(base / 2 + 1) + i;
+        sum += val;
+    }
+    sum
+}
+
+
+fn p2_invalid_sum(min: u64, max: u64, base_min: u32, base_max:u32) -> u64{
+    let mut sum: u64 = 0;
+    let mut seen: HashSet<u64> = HashSet::new();
+
+    for base in base_min..=base_max {
+        //Divisors to check
+        let divs = chunk_count(&base);
+        for divisor in divs {
+            let str_len = (base + 1) / divisor;
+            let min_val = 10u64.pow(str_len - 1);
+            let max_val = 10u64.pow(str_len) - 1;
+
+            for val in min_val..=max_val {
+                let int: u64 = reconstruct(val, divisor);
+                if int > max {
+                    break;
+                }
+                if int < min {
+                    continue;
+                }
+                if seen.insert(int) {
+                    sum += int
+                }
+            
+            }
+        }
+    }
+    sum
+}
+
+
+fn reconstruct(val: u64, divi: u32) -> u64 {
+    let mut sum: u64 = 0;
+    let base = val.to_string().len() as u32;
+    for _ in 0..divi {
+        sum *= 10u64.pow(base) as u64;
+        sum += val;
+    }
+    sum
+}
+
+
+fn chunk_count(base: &u32) -> Vec<u32> { //Returns possible lengths of sequences to check
+    let mut divs = Vec::new();
+    //Number of digits in val
+    let length = (base + 1) as u32;
+
+    for i in 2..=length {
+        //If i divides length
+        if (base + 1) % i == 0 {
+            divs.push(i);
+        }
+    }
+    divs
+}
